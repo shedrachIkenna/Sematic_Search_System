78fd0961f1e29e309a89c6c4b5bf5ac81b8a6c21
ShedrachIkenna <Shedrach686@gmail.com>
2025-10-11 21:03:31 +0100

    Added _compile_patterns function

--- src/preprocessing/text_cleaner.py
+++ src/preprocessing/text_cleaner.py
@@ -35,4 +35,38 @@ class TextPreprocessor:
         self.normalize_unicode = normalize_unicode
         self.min_length = min_length
 
+        # Compile regex patterns 
+        self._compile_patterns() 
+
+        # Statistics 
+        self.stats = {
+            'texts_processed': 0,
+            'texts_rejected': 0,
+            'avg_length_before': 0,
+            'avg_length_after': 0,
+            'total_chars_removed': 0
+        }
+
+        def _compile_patterns(self):
+            """Compile regex patterns for text processing"""
+            # URL pattern (matches http://, https://, www.)
+            self.url_pattern = re.compile(
+                r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+                r'|www\.(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+            )
+
+            # Email Pattern 
+            self.email_pattern = re.compile(
+                r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+            )
+
+            # Multiple spaces 
+            self.multiple_space_pattern = re.compile(r' {2,}')
+
+            # Multiple line breaks
+            self.multi_newline_pattern = re.compile(r'\n{3,}')
+        
+            # Special characters that should be replaced with space
+            self.special_chars_pattern = re.compile(r'[\t\r\f\v]')
+        
         
